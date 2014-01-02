995a3b962f44574c04af0b425df8baf79d96ae61
oefada <onjefu@gmail.com>
2014-01-02 13:53:14 -0800

    Modified for Parallels
    * Use the Parallels provider
    * Modified bootstrap file

--- Vagrantfile
+++ Vagrantfile
@@ -4,15 +4,6 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "precise32"
-    config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-    config.vm.network "private_network", ip: "192.168.50.10"
-    config.vm.synced_folder "../", "/vagrant", type: "nfs"
-    config.vm.provision :shell, :path => "bootstrap.sh"
-    
-    config.vm.provider "virtualbox" do |v|
-        v.customize ["modifyvm", :id, "--memory", "2048"]
-        v.customize ["modifyvm", :id, "--cpus", "2"]
-        v.customize ["modifyvm", :id, "--ioapic", "on"]
-    end
+    config.vm.box = "lldev-base"
+    config.vm.synced_folder "../", "/vagrant"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Upgrade base
-apt-get -y update > /dev/null 2>&1
-apt-get -y upgrade > /dev/null 2>&1
+apt-get -y update
+apt-get -y upgrade
 
 # Install required packages
-apt-get -y install php5 php5-curl php5-ldap php-apc php5-xdebug php5-memcached php5-memcache php5-mysql memcached libapache2-mod-rpaf nfs-common git unzip nginx ssh > /dev/null 2>&1
+apt-get -y install php5 php5-curl php5-ldap php-apc php5-xdebug php5-memcached php5-memcache php5-mysql memcached libapache2-mod-rpaf nfs-common git unzip nginx ssh
 a2enmod expires headers rewrite rpaf
 
 # Stop apache2 and nginx and backup base configs
@@ -16,15 +16,15 @@ mv /etc/apache2 /etc/apache2.bak
 mv /etc/php5 /etc/php5.bak
 
 # Grab our curated configs for apache2, nginx, and php5
-git clone http://lldev:lldev@git.luxurylink.com/scm/ldc/apache2.git /etc/apache2
-git clone http://lldev:lldev@git.luxurylink.com/scm/ldc/nginx.git /etc/nginx
-git clone http://lldev:lldev@git.luxurylink.com/scm/ldc/php5.git /etc/php5
+git clone http://git.luxurylink.com/scm/ldc/apache2.git /etc/apache2
+git clone http://git.luxurylink.com/scm/ldc/nginx.git /etc/nginx
+git clone http://git.luxurylink.com/scm/ldc/php5.git /etc/php5
 
 # Symlink docroots
-ln -s /vagrant/luxurylink /var/www/luxurylink
-ln -s /vagrant/appshared /var/www/appshared
-ln -s /vagrant/toolbox /var/www/toolbox
-ln -s /vagrant/vacationist /var/www/vacationist
+ln -s /media/psf/vagrant/luxurylink /var/www/luxurylink
+ln -s /media/psf/vagrant/appshared /var/www/appshared
+ln -s /media/psf/vagrant/toolbox /var/www/toolbox
+ln -s /media/psf/vagrant/vacationist /var/www/vacationist
 
 # Start services
 service apache2 start
@@ -44,10 +44,10 @@ echo "127.0.1.1 dev.api.luxurylink.com" >> /etc/hosts
 ### Application setup
 
 # Luxury Link
-ln -s /var/www/luxurylink/app/config/ConfigLL-DEV.php /var/www/luxurylink/app/config/ConfigLL.php
-ln -s /mnt/images /var/www/luxurylink/php/images
-mkdir /var/www/luxurylink/smarty/cache
-chmod 777 /var/www/luxurylink/smarty/cache /var/www/luxurylink/smarty/templates_c
+# ln -s /var/www/luxurylink/app/config/ConfigLL-DEV.php /var/www/luxurylink/app/config/ConfigLL.php
+# ln -s /mnt/images /var/www/luxurylink/php/images
+# mkdir /var/www/luxurylink/smarty/cache
+# chmod 777 /var/www/luxurylink/smarty/cache /var/www/luxurylink/smarty/templates_c
 
 # Toolbox
-ln -s /var/www/toolbox/app/config/database.php.dev-migration /var/www/toolbox/app/config/database.php
+# ln -s /var/www/toolbox/app/config/database.php.dev-migration /var/www/toolbox/app/config/database.php
